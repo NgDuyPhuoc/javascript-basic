0e13e5ed3a45d1349f9ae32f9e1ae2b99b5bec8f
NgDuyPhuoc <ngduyphuoc25082006@gmail.com>
2024-07-14 11:50:29 +0700

    update

---
+++ video4/video4.js
@@ -0,0 +1,16 @@
+let a = 'Tôi là string - chuỗi';
+console.log(a)
+console.log('a' + 10 +25)
+console.log(10 + 25 + 'a')
+
+
+let text = 'We are the so called "Vikings" from the north';
+console.log (text)
+let E = `I'm Nguyễn Duy Phước`
+console.log (E)
+let A = `Text xuống dòng 
+ABCDEF`;
+console.log (A)
+
+
+
