0418b902ca6a36aacbbcf1ae4795551c44268862
NgDuyPhuoc <ngduyphuoc25082006@gmail.com>
2024-07-15 09:27:54 +0700

    update

---
+++ video7/video7.js
@@ -0,0 +1,7 @@
+console.log('Hello world from HTML')
+let k = 10;
+let b = 25;
+console.log('Test Strings Template: ' + (k + b) + ' and\nnot ' + (2 * k + b) + '.');
+
+console.log(`Test Strings Template: ${k + b} and
+not ${2 * k + b}.`)
