8ceab8be8e372911b5a919570c76f256184e7100
NgDuyPhuoc <ngduyphuoc25082006@gmail.com>
2024-07-16 09:58:44 +0700

    update

--- video7/video7.js
+++
@@ -1,7 +0,0 @@
-console.log('Hello world from HTML')
-let k = 10;
-let b = 25;
-console.log('Test Strings Template: ' + (k + b) + ' and\nnot ' + (2 * k + b) + '.');
-
-console.log(`Test Strings Template: ${k + b} and
-not ${2 * k + b}.`)
---
+++ video8/video8.js
@@ -0,0 +1,21 @@
+console.log('Hello world from HTML')
+
+
+let a = {};
+let b = 'Bi';
+console.log(`type of a: ${typeof a}
+type of b: ${typeof b}`)
+
+
+let Bi = {
+    Name: 'Nguyen Duy Phuoc',
+    Old: '18',
+    Address: 'Tra Vinh',
+}
+console.log(`What your name? 
+- My name is ${Bi.Name}`)
+console.log(`How old are you?
+- I'm ${Bi.Old} years old`)
+console.log(`Where are you from?
+- I'm from ${Bi.Address}`)
+
