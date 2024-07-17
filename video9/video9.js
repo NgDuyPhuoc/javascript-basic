cb51f29103d7402a66e1e1b0029b75691d39ed86
NgDuyPhuoc <ngduyphuoc25082006@gmail.com>
2024-07-17 17:17:31 +0700

    update

---
+++ video9/video9.js
@@ -0,0 +1,21 @@
+console.log('Hello world from HTML')
+
+//Object: Key, value:
+let Bi = {
+    name: 'Nguyen Duy Phuoc',
+    age: '18',
+};
+console.log('Check data object:', Bi)
+console.log('My name\'s: ', Bi.name)
+
+//Array:
+let a = ['AB', 'BC', 'CD'];
+console.log('type of a:', typeof a, a)
+console.log(`First item: ${a[0]}
+Second item: ${a[1]}`)
+
+let family = [
+    {name: 'AAA', age: '22'},
+    {name: 'BBB', age: '21'}
+]
+console.log('My family: ', family)
