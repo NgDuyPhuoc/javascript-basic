4a3dddb0c763ab702fdb2b714b2cbb17c52db180
NgDuyPhuoc <ngduyphuoc25082006@gmail.com>
2024-07-18 18:09:31 +0700

    update

---
+++ video10/video10.js
@@ -0,0 +1,8 @@
+console.log('Hello world from HTML')
+let a = 10; // x <-- y
+let b = '10';
+console.log('compare a and b:', 'a= ', a, 'b= ', b, 'result: ', a===b)
+
+//empty, null, undefined
+let c = null;
+console.log('Check value:', c)
