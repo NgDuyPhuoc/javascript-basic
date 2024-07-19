75e83e0a3c019b884d63586d2944b5302651068c
NgDuyPhuoc <ngduyphuoc25082006@gmail.com>
2024-07-19 09:02:32 +0700

    update

---
+++ video11/video11.js
@@ -0,0 +1,10 @@
+console.log('Hello world from HTML')
+
+let arr = ['ABC', 'abc', 'BCD', 'bcd']
+
+let i = 0;
+
+for(let i = 0; i < arr.length; i++){
+    console.log('check variable i = ', arr[i])
+}
+console.log('check value i: ', i)
