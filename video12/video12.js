1f0cbf22c9269aad584a3ab58dec0b657d331fc7
NgDuyPhuoc <ngduyphuoc25082006@gmail.com>
2024-07-20 10:17:22 +0700

    update

---
+++ video12/video12.js
@@ -0,0 +1,16 @@
+console.log('Hello world from HTML')
+let arr = ['ABC', 'abc', 'BCD', 'bcd'];
+//for(let a = 0; a < arr.length; a++){
+//    console.log('Top:', a + 1 , arr[a])
+//}
+
+let i = 0;
+//while(i < arr.length) {
+//    console.log('Top:', i + 1, arr[i],);
+//    i++
+//}
+
+do{
+    console.log('Top:', i + 1, arr[i]);
+    i++;
+} while(i < 0)
