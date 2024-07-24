12c8e2b446b2d98586996b2828e44ae1fa3ca005
NgDuyPhuoc <ngduyphuoc25082006@gmail.com>
2024-07-24 18:03:39 +0700

    update

--- video15/video15.js
+++ video15/video15.js
@@ -3,8 +3,8 @@ console.log('Hello world from HTML')
 //console.log('Check value: ', x)
 //let x = 5;
 
-console.log('Check value var: ', y)
-var y = 10;
+//console.log('Check value var: ', y)
+//var y = 10;
 
 for(let i = 0; i < 10; i++){
     console.log('Check value i: ', i)
---
+++ video16/video16.js
@@ -0,0 +1,8 @@
+console.log('Hello world from HTML')
+
+function sum(a, b){
+    return a + b;
+}
+//console.log('Sum a + b = ', sum(25, 10))
+let pk = sum(25, 10)
+console.log('Sum pk = ', pk)
