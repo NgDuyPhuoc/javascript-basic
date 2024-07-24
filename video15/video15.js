8c79c4dbbf0ac4d67539697fecfc8c8d414a4dd9
NgDuyPhuoc <ngduyphuoc25082006@gmail.com>
2024-07-24 17:28:40 +0700

    update

---
+++ video15/video15.js
@@ -0,0 +1,11 @@
+console.log('Hello world from HTML')
+
+//console.log('Check value: ', x)
+//let x = 5;
+
+console.log('Check value var: ', y)
+var y = 10;
+
+for(let i = 0; i < 10; i++){
+    console.log('Check value i: ', i)
+}
