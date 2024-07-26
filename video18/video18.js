bc63d53abe489ee50c4b546df8e687ce43144ed9
NgDuyPhuoc <ngduyphuoc25082006@gmail.com>
2024-07-27 03:53:06 +0700

    update

---
+++ video18/video18.js
@@ -0,0 +1,10 @@
+console.log('Hello world from HTML')
+//function Sum(a, b){
+//    return a + b;
+//}
+
+//arrow function:
+let Sum = (a, b) => {
+    return a + b;
+}
+console.log('Check Sum: ', Sum(25, 10))
